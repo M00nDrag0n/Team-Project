4d4a461f8bb1b0cf6648204117a6aadb859e51bb
M00nDrag0n <48165601+M00nDrag0n@users.noreply.github.com>
2019-07-15 20:17:29 -0700

    Add files via upload
    added code to JS for a functioning return with a hidden img from the selectors

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("form#monster").submit(function(event) {
+    event.preventDefault();
+    var bodyParts = "";
+    var wings = "Wings";
+    var legs = "Legs";
+    var arms =" Arms";
+    var eye = "Eyes";
+    var spikyFeatures = ["Scales", "horns"];
+  
+
+
+ $("#result").show();
+
+});
+});
