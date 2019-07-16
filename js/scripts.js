58d8d21213427064d81de3a0b28a12b357052a8e
M00nDrag0n <48165601+M00nDrag0n@users.noreply.github.com>
2019-07-15 20:44:00 -0700

    Add files via upload
    added to JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
   $("form#monster").submit(function(event) {
     event.preventDefault();
-    var bodyParts = "";
-    var wings = "Wings";
-    var legs = "Legs";
-    var arms =" Arms";
-    var eye = "Eyes";
-    var spikyFeatures = ["Scales", "horns"];
-  
+
+  // >>>>>>>> Business Logic <<<<<<<<<
+    var bodyParts = function(){
+      if (wings === true)
+        return
+        //https://www.learnhowtoprogram.com/intro-to-programming-evening/arrays-looping-8a0d105a-0895-42ec-b85c-d45f7f2eba79/leap-year-part-2
+        $("")
+      display = "hey"
+    }
+// >>>>>>>>  User Interface Logic <<<<<<<<<<<
+
+var wings = "Wings";
+var legs = "Legs";
+var arms =" Arms";
+var eye = "Eyes";
+var spikyFeatures = ["Scales", "horns"];
+
+
 
 
  $("#result").show();
