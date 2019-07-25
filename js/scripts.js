459381daad36a30d211d0766a759b7a7c0f2c308
Hailey J Gaylor <48165448+Hgaylor0220@users.noreply.github.com>
2019-07-24 19:48:29 -0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,16 @@ $(document).ready(function() {
   $("form#monster").submit(function(event) {
     event.preventDefault();
     var wingsImageSrc = $("input:radio[name=wings]:checked").val();
-    var legsImageSrc = $("input:radio[name=legs]:checked").val();
-    var armsImageSrc = $("input:radio[name=arms]:checked").val();
+    var eyesImageSrc = $("input:radio[name=eyes]:checked").val();
+    var mouthImageSrc = $("input:radio[name=mouth]:checked").val();
+    var clawsImageSrc = $("input:radio[name=claws]:checked").val();
 
 
 
-  $("#result").append("<img src=" + wingsImageSrc + ">")
+
+  $("#result") .append( wingsImageSrc + eyesImageSrc + mouthImageSrc + clawsImageSrc + "  This is your Monster!!")
+  $('.container').hide();
+  $("#result").hide();
   })
 
 });
